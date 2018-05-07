3a51a23720953edb27f696e72eaec376afc4a21a
chao-ji <jic@indiana.edu>
2018-05-07 20:59:33 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,22 +1,45 @@
+"""The preactivation variant of Residual networks for classifying 32 by 32
+images from CIFAR10 dataset.
+
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+
+The full preactivation 'v2' ResNet variant implemented in this module was
+introduced by:
+[2] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Identity Mappings in Deep Residual Networks. arXiv: 1603.05027
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
 import tensorflow as tf
-from tensorflow.python.layers import layers
+from tensorflow.contrib import layers as layers_lib
+from tensorflow.contrib.framework.python.ops import add_arg_scope
+from tensorflow.contrib.framework.python.ops import arg_scope
+from tensorflow.contrib.layers.python.layers import layers
+from tensorflow.contrib.layers.python.layers import utils
 
 import data
+import resnet_utils
 
-BatchNorm = layers.BatchNormalization
-Conv = layers.Conv2D
-Dense = layers.Dense
-AvgPool = layers.AveragePooling2D
+resnet_arg_scope = resnet_utils.resnet_arg_scope
+
 
 class ResNetModel(object):
-  def __init__(self, hparams, dataset, mode, residual_connection=True):
+  """ResNet Model builder."""
+  def __init__(self, hparams, dataset, mode):
     self._mode = mode
-    self._filters = hparams.num_filters 
-    self._num_blocks = hparams.num_blocks
-    self._training = (self._mode == tf.contrib.learn.ModeKeys.TRAIN)
-    self._residual_connection = residual_connection
+    self._is_training = self._mode == tf.contrib.learn.ModeKeys.TRAIN
+    self._shortcut_conn = hparams.shortcut_conn
     self._batch_size = tf.size(dataset.labels)
-    self.logits = self._build_graph(hparams, dataset)
+
+    with arg_scope(resnet_arg_scope(
+        weight_decay=hparams.weight_decay,
+        batch_norm_epsilon=hparams.epsilon,
+        random_seed=hparams.random_seed)):
+      self.logits = self._build_graph(hparams, dataset)
 
   @property
   def mode(self):
@@ -27,131 +50,95 @@ class ResNetModel(object):
     return self._batch_size
 
   @property
-  def residual_connection(self):
-    return self._residual_connection
+  def shortcut_conn(self):
+    return self._shortcut_conn
 
-  def _build_graph(self, hparams, dataset):
-    inputs = dataset.images
+  def _build_graph(self, hparams, dataset, reuse=None):
+    """Builds the graph for the forward pass (from `dataset.inputs` to `logits`)
+    """ 
+    inputs = dataset.inputs
+    num_layers = hparams.num_layers
+    shortcut_conn = self.shortcut_conn
+    is_training = self._is_training
 
-    with tf.variable_scope("Pre_Residual_Layer"):
-      ftmps = self._conv_bn_relu(inputs, self._filters[0], (1, 1), hparams)
-       
-    for i in range(self._num_blocks):
-      with tf.variable_scope("Filters_%d_Block_%d" % (self._filters[0], i)):
-        ftmps = self._residual_block(
-            ftmps, self._filters[0], self._filters[0], hparams, i == 0)
+    if num_layers not in (20, 32, 44, 56, 110):
+      raise ValueError("`num_layers' must be 20, 32, 44, 56 or 100.")
 
-    for i in range(self._num_blocks):
-      with tf.variable_scope("Filters_%d_Block_%d" % (self._filters[1], i)):
-        ftmps = self._residual_block(
-            ftmps, self._filters[0] if i == 0 else self._filters[1],
-            self._filters[1], hparams, False)
+    num_units = (num_layers - 2) // 6
+    scope = "resnet_v2_cifar10_%d" % num_layers
 
-    for i in range(self._num_blocks):
-      with tf.variable_scope("Filters_%d_Block_%d" % (self._filters[2], i)):
-        ftmps = self._residual_block(
-            ftmps, self._filters[1] if i == 0 else self._filters[2],
-            self._filters[2], hparams, False)
+    blocks = [resnet_v2_block("block1", num_units, 16, 1, shortcut_conn, True),
+        resnet_v2_block("block2", num_units, 32, 2, shortcut_conn, False),
+        resnet_v2_block("block3", num_units, 64, 2, shortcut_conn, False)]
 
-    with tf.variable_scope("Post_Residual_Layer"):
-      logits = self._final_block(ftmps, hparams.num_classes, hparams)
+    with tf.variable_scope(scope, "resnet_v2", [inputs], reuse=reuse) as sc:
+      with arg_scope([layers.batch_norm], is_training=is_training):
+        net = inputs
+
+        net = layers_lib.conv2d(net, 16, 3, 1, "SAME", 
+            activation_fn=None,
+            normalizer_fn=None,
+            biases_initializer=None,
+            scope="init_conv")
+
+        net = resnet_utils.stack_blocks(net, blocks)
+
+        net = layers.batch_norm(
+            net, activation_fn=tf.nn.relu, scope="postnorm")
+        net = tf.reduce_mean(
+            net, [1, 2], name="global_average_pooling", keepdims=True)
+        net = layers_lib.conv2d(net, 10, 1, 1, 
+            activation_fn=None,
+            normalizer_fn=None,
+            weights_initializer=tf.initializers.variance_scaling(
+                distribution="uniform",
+                seed=hparams.random_seed),
+            scope="logits")
+        logits = tf.squeeze(net, axis=[1, 2])
 
     return logits
 
-  def _conv_bn_relu(self, inputs, filters, strides, hparams):
-    conv_layer = Conv(filters=filters,
-                      kernel_size=hparams.kernel_size,
-                      strides=strides,
-                      padding="SAME",
-                      use_bias=False,
-                      kernel_initializer=_conv_kernel_initializer(hparams),
-                      kernel_regularizer=_regularizer(hparams))
-    batch_norm_layer = BatchNorm(axis=-1,
-                                 fused=False,
-                                 epsilon=hparams.epsilon)
 
-    ftmps = conv_layer(inputs)   
-    ftmps = batch_norm_layer(ftmps, training=self._training)
-    ftmps = tf.nn.relu(ftmps)
-    return ftmps
+@add_arg_scope
+def unit_fn_v2(inputs,
+               depth,
+               stride,
+               shortcut_conn,
+               shortcut_from_preact,
+               scope=None):
+  """The v2 variant of ResNet unit with BN-relu preceeding convolution.
+  See ref [2].
+  """
+  with tf.variable_scope(scope, "unit_fn_v2", [inputs]) as sc:
+    depth_in = utils.last_dimension(inputs.get_shape(), min_rank=4)
+    preact = layers.batch_norm(
+        inputs, activation_fn=tf.nn.relu, scope="preact")
 
-  def _residual_block(self, inputs, in_depth, out_depth, hparams, first_block):
-    ftmps = inputs 
-    batch_norm_layers = [BatchNorm(axis=-1,
-                                   fused=False,
-                                   epsilon=hparams.epsilon),
-                         BatchNorm(axis=-1,
-                                   fused=False,
-                                   epsilon=hparams.epsilon)]
-    conv_layers = [Conv(filters=out_depth,
-                        kernel_size=hparams.kernel_size,
-                        strides=(1, 1) if in_depth == out_depth else (2, 2),
-                        padding="SAME",
-                        use_bias=False,
-                        kernel_initializer=_conv_kernel_initializer(hparams),
-                        kernel_regularizer=_regularizer(hparams)),
-                   Conv(filters=out_depth,
-                        kernel_size=hparams.kernel_size,
-                        strides=(1, 1),
-                        padding="SAME",
-                        use_bias=False,
-                        kernel_initializer=_conv_kernel_initializer(hparams),
-                        kernel_regularizer=_regularizer(hparams))]
-    avg_pool_layer = AvgPool(pool_size=(2, 2),
-                             strides=(2, 2),
-                             padding="VALID") 
+    shortcut = preact if shortcut_from_preact else inputs
 
-    with tf.variable_scope("ConvLayer1"):
-      if not first_block:
-        ftmps = batch_norm_layers[0](ftmps, training=self._training)
-        ftmps = tf.nn.relu(ftmps)
-      ftmps = conv_layers[0](ftmps)
+    if depth != depth_in:
+      with tf.name_scope("shortcut"):
+        shortcut = layers.avg_pool2d(shortcut, [2, 2], stride=2)
+        shortcut = tf.pad(
+            shortcut, [[0, 0], [0, 0], [0, 0], [(depth - depth_in) // 2] * 2])
 
-    with tf.variable_scope("ConvLayer2"):
-      ftmps = batch_norm_layers[1](ftmps, training=self._training)
-      ftmps = tf.nn.relu(ftmps)
-      ftmps = conv_layers[1](ftmps)
+    residual = layers_lib.conv2d(preact, depth, 3, stride, scope="conv1")
+    residual = layers_lib.conv2d(residual, depth, 3, 1,
+        normalizer_fn=None, activation_fn=None, biases_initializer=None,
+        scope="conv2")
 
-    if self.residual_connection:
-      if in_depth == out_depth:
-        padded_inputs = inputs
-      else:
-        pooled_inputs = avg_pool_layer(inputs)
-        padded_inputs = tf.pad(pooled_inputs,
-            [[0, 0], [0, 0], [0, 0], [(out_depth - in_depth) / 2] * 2])
-      return ftmps + padded_inputs
-    else:
-      return ftmps
+    output = residual + shortcut if shortcut_conn else residual
 
-  def _final_block(self, inputs, num_units, hparams):
-    batch_norm_layer = BatchNorm(axis=-1,
-                                 fused=False,
-                                 epsilon=hparams.epsilon)
-    dense_layer = Dense(num_units,
-                        use_bias=True,
-                        kernel_initializer=_dense_kernel_initializer(hparams),
-                        kernel_regularizer=_regularizer(hparams))
+    return output 
 
-    ftmps = batch_norm_layer(inputs, training=self._training)
-    ftmps = tf.nn.relu(ftmps)
-    ftmps_global_avg_pool = tf.reduce_mean(ftmps, reduction_indices=[1, 2])
-    logits = dense_layer(ftmps_global_avg_pool)
-    return logits
 
-def _compute_loss(logits, labels):
-  xentropy_loss = tf.reduce_mean(
-      tf.nn.sparse_softmax_cross_entropy_with_logits(
-          labels=labels, logits=logits))
-  regularization_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-  total_loss = tf.add_n([xentropy_loss] + regularization_loss)
-  return total_loss
-
-def _conv_kernel_initializer(hparams):
-  return tf.contrib.layers.xavier_initializer(seed=hparams.random_seed)
-
-def _dense_kernel_initializer(hparams):
-  return tf.initializers.variance_scaling(
-      distribution="uniform", seed=hparams.random_seed)
-
-def _regularizer(hparams):
-  return tf.contrib.layers.l2_regularizer(scale=hparams.weight_decay)
+def resnet_v2_block(
+    scope, num_units, depth, stride, shortcut_conn, shortcut_from_preact):
+  """Helper for creating ResNet v2 block definition."""
+  args = [{
+      "depth": depth,
+      "stride": stride if i == 0 else 1,
+      "shortcut_conn": shortcut_conn,
+      "shortcut_from_preact": shortcut_from_preact if i == 0 else False}
+      for i in range(num_units)]
+  return resnet_utils.Block(scope, unit_fn_v2, args)
